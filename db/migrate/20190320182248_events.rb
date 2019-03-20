32cc3fd1d1e4875213c1a3f7713333c20f2ad7bd
carlaight <charlesmix23@gmail.com>
2019-03-20 19:28:02 +0100

    Events Table DONE

--- db/migrate/20190320182248_events.rb
+++ db/migrate/20190320182248_events.rb
@@ -1,4 +1,21 @@
 class Events < ActiveRecord::Migration[5.2]
   def change
+    create_table :events do |t|
+      t.string :name
+      t.text :description
+      t.datetime :start_date
+      t.integer :duration
+      t.string :spectator_price
+      t.text :rules
+      t.string :prize_money
+      t.string :picture_url
+      t.string :city_name
+      t.string :zipcode
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.belongs_to :promoter, index: true
+      t.timestamps
+    end
   end
 end
