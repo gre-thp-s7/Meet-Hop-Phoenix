ecedf4b0c4e22da958a527b698ba7bd42a58720f
carlaight <charlesmix23@gmail.com>
2019-03-20 20:11:28 +0100

    Charges controler OK

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,47 @@
 class ChargesController < ApplicationController
-end
+  def new
+    params.permit!
+    @dancer = params[:dancer]
+    @categories = params[:categories]
+    @event = Event.find(params[:event_id])
+
+    if @dancer == 'true'
+      puts "le prix dancer #{@event.dancer_price}"
+      @amount = @event.dancer_price.to_i * 100
+    else
+      puts "le prix spectateur #{@event.spectator_price}"
+      @amount = @event.spectator_price.to_i * 100
+    end
+  end
+
+  def create
+    params.permit!
+
+    @user_id = current_user.id
+
+    @event = Event.find(params[:event_id])
+
+    if @dancer == 'true'
+      puts "le prix dancer #{@event.dancer_price}"
+      @amount = @event.dancer_price
+    else
+      puts "le prix spectateur #{@event.spectator_price}"
+      @amount = @event.spectator_price
+    end
+
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount.to_i * 100,
+      description: 'Rails Stripe customer',
+      currency: 'eur'
+    )
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_event_charge_path
+    end
+  end
